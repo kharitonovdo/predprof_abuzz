a3df4b07c4a99daa038528ff1ec0411586b5a4e8
dolotova <o.dolotova@yandex.ru>
2025-03-04 17:16:30 +0300

    Merge branch 'main' of https://github.com/kharitonovdo/predprof_abuzz qwe
    Signed-off-by: dolotova <o.dolotova@yandex.ru>

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
+@app.route('/index')
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
