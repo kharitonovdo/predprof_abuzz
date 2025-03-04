79b83269b225cbe141b51e51e355c8401c179ec1
Дмитрий <kharitonovdo@yandex.ru>
2025-03-04 17:07:24 +0300

    goida

---
+++ static/js/master.js
@@ -0,0 +1,26 @@
+let a = [[1,0,1],
+         [0,0,1],
+         [1,0,1]
+        ]
+
+function createTable(tableData) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
+  
+    tableData.forEach(function(rowData) {
+      var row = document.createElement('tr');
+  
+      rowData.forEach(function(cellData) {
+        var cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(cellData));
+        row.appendChild(cell);
+      });
+  
+      tableBody.appendChild(row);
+    });
+    
+    table.appendChild(tableBody);
+    document.body.appendChild(table);
+  }
+  
+  createTable(a);
